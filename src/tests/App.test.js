7c259e4a73bf08bdddb13043a545de768535f085
xWesleyMartins <wesley_869@hotmail.com.br>
2022-07-18 17:58:42 -0300

    completando requisito 1(obrigatorios)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  test(`Teste se o topo da aplicação
+  contém um conjunto fixo de links de navegação`, () => {
+    renderWithRouter(<App />);
+
+    const linkToHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkToHome).toBeInTheDocument();
+
+    const linkToAbaut = screen.getByRole('link', { name: /About/i });
+    expect(linkToAbaut).toBeInTheDocument();
+
+    const linkToFavPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkToFavPokemons).toBeInTheDocument();
+
+    // const po
+  });
+});
