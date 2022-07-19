c019a30e3c8d127d03c15369fef2173a285b5948
xWesleyMartins <wesley_869@hotmail.com.br>
2022-07-18 21:05:17 -0300

    completando requisito 3(obrigatorios)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const textNoFoundPokemon = screen
+      .getByText(/No favorite pokemon found/i);
+
+    expect(textNoFoundPokemon).toBeInTheDocument();
+  });
+});
